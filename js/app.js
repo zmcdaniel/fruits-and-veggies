c39cbf4f4cb42556b07e4ba26890d62e43d8b7ef
zmcdaniel <zmcdaniel@gmail.com>
2016-09-18 17:34:46 -0700

    Sort of sorting it

--- js/app.js
+++ js/app.js
@@ -1,6 +1,27 @@
 /* setup your angular app here */
 
-// debug stuff to show the app is loading and fruit / veggies are available
-console.log('App Started');
-console.log('Fruit count', fruits.length);
-console.log('Veggie count', vegetables.length);
+var app = angular.module('FruitVeggieApp', []);
+
+app.run(function(){
+    // debug stuff to show the app is loading and fruit / veggies are available
+    console.log('App Started');
+    console.log('Fruit count', fruits.length);
+    console.log('Veggie count', vegetables.length);
+});
+
+app.controller("FruitVeggieCtrl", ["$scope", function($scope){
+    $scope.allProduce = vegetables.concat(fruits);
+    $scope.fruits = [];
+    $scope.veggies = [];
+
+    $scope.moveLeft = function(index){
+        $scope.fruits.push($scope.allProduce[index]);
+        $scope.allProduce.splice(index, 1);
+    };
+
+    $scope.moveRight = function(index){
+        $scope.veggies.push($scope.allProduce[index]);
+        $scope.allProduce.splice(index, 1);
+    };
+
+}]);
